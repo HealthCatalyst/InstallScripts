cf72924b6a2c09b6a8656a081099b086ee4f5d63
michael.vidal <michael.vidal@healthcatalyst.com>
2017-06-12 11:11:23 -0400

    Tweaked the network setup in the compose script for auth. Added a script to install couch in swarm.

---
+++ configure-couch-cluster.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+couchnode=$1
+couchport=$2
+couchport2=$3
+couchport3=$4
+
+echo "enable cluster"
+curl "http://$COUCHDB_USER:$COUCHDB_PASSWORD@$couchnode:$couchport/_cluster_setup" -H 'Content-Type: application/json' -H 'Accept: application/json' \
+	--data-binary "{\"action\":\"enable_cluster\",\"username\":\"$COUCHDB_USER\",\"password\":\"$COUCHDB_PASSWORD\",\"bind_address\":\"0.0.0.0\",\"port\":5984}" --compressed
+
+echo "enable cluster for couchd2"
+curl "http://$COUCHDB_USER:$COUCHDB_PASSWORD@$couchnode:$couchport/_cluster_setup" -H 'Content-Type: application/json' -H 'Accept: application/json'  \
+	--data-binary "{\"action\":\"enable_cluster\",\"username\":\"$COUCHDB_USER\",\"password\":\"$COUCHDB_PASSWORD\",\"bind_address\":\"0.0.0.0\",\"port\":5984,\"remote_node\":\"couchdb2\",\"remote_current_user\":\"$COUCHDB_USER\",\"remote_current_password\":\"$COUCHDB_PASSWORD\"}" --compressed
+
+./wait-for-it.sh $couchnode:$couchport2 -t 300 -- echo "couchdb2 is up"
+
+echo "add node couchdb2"
+curl "http://$COUCHDB_USER:$COUCHDB_PASSWORD@$couchnode:$couchport/_cluster_setup" -H 'Content-Type: application/json' -H 'Accept: application/json' \
+	--data-binary "{\"action\":\"add_node\",\"username\":\"$COUCHDB_USER\",\"password\":\"$COUCHDB_PASSWORD\",\"host\":\"couchdb2\",\"port\":5984}" --compressed
+
+echo "enable cluster for couchdb3"
+curl "http://$COUCHDB_USER:$COUCHDB_PASSWORD@$couchnode:$couchport/_cluster_setup" -H 'Content-Type: application/json' -H 'Accept: application/json' \
+	--data-binary "{\"action\":\"enable_cluster\",\"username\":\"$COUCHDB_USER\",\"password\":\"$COUCHDB_PASSWORD\",\"bind_address\":\"0.0.0.0\",\"port\":5984,\"remote_node\":\"couchdb3\",\"remote_current_user\":\"$COUCHDB_USER\",\"remote_current_password\":\"$COUCHDB_PASSWORD\"}" --compressed
+
+./wait-for-it.sh $couchnode:$couchport3 -t 300 -- echo "couchdb3 is up"
+
+echo "add node for couchdb3"
+curl "http://$COUCHDB_USER:$COUCHDB_PASSWORD@$couchnode:$couchport/_cluster_setup" -H 'Content-Type: application/json' -H 'Accept: application/json' \
+	--data-binary "{\"action\":\"add_node\",\"username\":\"$COUCHDB_USER\",\"password\":\"$COUCHDB_PASSWORD\",\"host\":\"couchdb3\",\"port\":5984}" --compressed
+
+echo "finish cluster"
+curl "http://$COUCHDB_USER:$COUCHDB_PASSWORD@$couchnode:$couchport/_cluster_setup" -H 'Content-Type: application/json' -H 'Accept: application/json' \
+	--data-binary "{\"action\":\"finish_cluster\"}" --compressed
+
---
+++ installcouchdbcluster-swarm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+docker network create --driver overlay dbnet
+
+docker service create --name  couchdb1 \
+	--env NODENAME=couchdb1 \
+	--env COUCHDB_USER=$COUCHDB_USER \
+	--env COUCHDB_PASSWORD=$COUCHDB_PASSWORD \
+	-p 15984:5984 \
+	--network dbnet \
+	healthcatalyst/fabric.docker.couchdb
+
+docker service create --name couchdb2 \
+	--env NODENAME=couchdb2 \
+	--env COUCHDB_USER=$COUCHDB_USER \
+	--env COUCHDB_PASSWORD=$COUCHDB_PASSWORD \
+	-p 25984:5984 \
+	--network dbnet \
+	healthcatalyst/fabric.docker.couchdb
+
+docker service create --name couchdb3 \
+	--env NODENAME=couchdb3 \
+	--env COUCHDB_USER=$COUCHDB_USER \
+	--env COUCHDB_PASSWORD=$COUCHDB_PASSWORD \
+	-p 35984:5984 \
+	--network dbnet \
+	healthcatalyst/fabric.docker.couchdb
+
+sleep 30
+
+./configure-couch-cluster.sh localhost 15984 25984 35984
+
+docker service create --name couchproxy \
+	-p 5984:5984 \
+	--network dbnet \
+	healthcatalyst/fabric.docker.haproxy

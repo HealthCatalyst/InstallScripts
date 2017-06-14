6fec1fd8745b8ca3b7b59639c87a31af6b71153a
michael.vidal <michael.vidal@healthcatalyst.com>
2017-06-14 11:53:38 -0400

    Updated fabric.identity service create script to set secrets for token signing.

--- setupidentity-authcluster-swarm.sh
+++ setupidentity-authcluster-swarm.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 4 ]; then
 	echo "You must specify CouchDB User as first argument and CouchDB Password as second argument"
 else
 	export COUCHDB_USER=$1
 	export COUCHDB_PASSWORD=$2
+	pfxpassword=$3
+	pfxpath=$4
 	./installcouchdbcluster-swarm.sh
-	./installidentity-swarm.sh http://couchproxy:5984
+	./installidentity-swarm.sh http://couchproxy:5984 $pfxpassword $pfxpath
 	./installauthorization-swarm.sh http://identity:5001
 fi
 

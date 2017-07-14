78ca3a9653ec540ce2863cc872b4e47ff7e24382
michael.vidal <michael.vidal@healthcatalyst.com>
2017-07-14 10:45:23 -0400

    Use 127.0.0.1 because localhost is not always defined.

--- installcouchdbcluster-swarm.sh
+++ installcouchdbcluster-swarm.sh
@@ -76,7 +76,7 @@ echo "waiting for couchdb nodes to come up"
 sleep 30
 
 echo "configuring couch cluster"
-curl -sSL https://healthcatalyst.github.io/InstallScripts/configure-couch-cluster.sh | sh /dev/stdin localhost 15984 25984 35984
+curl -sSL https://healthcatalyst.github.io/InstallScripts/configure-couch-cluster.sh | sh /dev/stdin 127.0.0.1 15984 25984 35984
 
 
 echo "creating couch ha proxy service"

a39d5b189f40f499b6b359bc696ca3c98c974ebb
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-10-25 19:15:46 -0700

    specify CLIENT_CERT_USERNAME
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- realtime/runondevmachine.sh
+++ realtime/runondevmachine.sh
@@ -12,16 +12,18 @@ echo 'roboconf2' | docker secret create RabbitMqMgmtUiPassword -
 docker secret rm MySQLPassword || echo ""
 echo 'roboconf2' | docker secret create MySQLPassword -
 
-export CERT_HOSTNAME=IAmAHost
-export CERT_PASSWORD=roboconf2
-export MySQLRootPassword=roboconf2
+docker secret rm CertHostName || echo ""
+echo "localrealtime" |  docker secret create CertHostName -
+
+docker secret rm MySQLRootPassword || echo ""
+echo "roboconf2" |  docker secret create MySQLRootPassword -
+
 export SHARED_DRIVE=c:/tmp
 export SHARED_DRIVE_CERTS=c:/tmp/certs
 export SHARED_DRIVE_RABBITMQ=c:/tmp/rabbitmq
 export SHARED_DRIVE_MYSQL=c:/tmp/mysql
 
 # docker-compose -f realtime-stack.yml up
-
 
 docker stack deploy -c realtime-stack.yml fabricrealtime
 

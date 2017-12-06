d07ab5b5cd01febab8b4dbfaa9ed8e4b089f75e9
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-12-05 16:26:10 -0800

    fix nl-kubernetes.yml
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

---
+++ nlp/runkondevmachine.sh
@@ -0,0 +1,14 @@
+
+
+kubectl delete -f ./nlp-kubernetes.yml
+kubectl delete namespace fabricnlp
+
+kubectl create namespace fabricnlp
+
+kubectl create secret generic mysqlrootpassword --namespace=fabricnlp --from-literal=password=ILoveNLP2017!
+
+kubectl create secret generic mysqlpassword --namespace=fabricnlp --from-literal=password=ILoveNLP2017!
+
+kubectl create secret generic certhostname --namespace=fabricnlp --from-literal=value=imran.com
+
+kubectl create secret generic certpassword --namespace=fabricnlp --from-literal=password=ILoveNLP2017!

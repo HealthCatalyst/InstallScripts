2259157ff0c9c7ff0b89862b7efee4a5ececdea6
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2018-02-14 20:51:51 -0800

    renumber menu items
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- kubernetes/main.sh
+++ kubernetes/main.sh
@@ -5,7 +5,7 @@ set -e
 #   curl -sSL https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/kubernetes/main.sh | bash
 #
 #
-version="2018.02.13.06"
+version="2018.02.14.01"
 
 GITHUB_URL="https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master"
 
@@ -21,22 +21,23 @@ while [[ "$input" != "q" ]]; do
     echo "------ Install -------"
     echo "1: Add this VM as Master"
     echo "2: Add this VM as Worker"
-    echo "3: Setup Load Balancer"
-    echo "4: Install NLP"
-    echo "5: Install Realtime"
+    echo "3. Join a new node to this cluster"
+    echo "4: Setup Load Balancer"
+    echo "5: Install NLP"
+    echo "6: Install Realtime"
     echo "----- Troubleshooting ----"
-    echo "6: Show status of cluster"
-    echo "6: Launch Kubernetes Admin Dashboard"
-    echo "8: View status of DNS pods"
-    echo "9: Apply updates and restart all VMs"
+    echo "11: Show status of cluster"
+    echo "12: Launch Kubernetes Admin Dashboard"
+    echo "13: View status of DNS pods"
+    echo "14: Apply updates and restart all VMs"
     echo "------ NLP -----"
-    echo "10: Show status of NLP"
-    echo "11: Test web sites"
-    echo "12: Show passwords"
-    echo "13: Show NLP logs"
-    echo "14: Restart NLP"
+    echo "21: Show status of NLP"
+    echo "22: Test web sites"
+    echo "23: Show passwords"
+    echo "24: Show NLP logs"
+    echo "25: Restart NLP"
     echo "------ Realtime -----"
-    echo "15: Show status of realtime"
+    echo "31: Show status of realtime"
     echo "-----------"
     echo "q: Quit"
 
@@ -48,30 +49,34 @@ while [[ "$input" != "q" ]]; do
         ;;
     2) curl -sSL https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/kubernetes/setupnode.txt | bash
         ;;
-    3)  curl -sSL https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/kubernetes/setup-loadbalancer.sh | bash
+    3)  echo "Run this command on the new node to join this cluster:"
+        sudo kubeadm token create --print-join-command
         ;;
-    4)  curl -sSL https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/nlp/installnlpkubernetes.sh | bash
+    4)  curl -sSL https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/kubernetes/setup-loadbalancer.sh | bash
         ;;
-    6)  echo "Current cluster: $(kubectl config current-context)"
+    5)  curl -sSL https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/master/nlp/installnlpkubernetes.sh | bash
+        ;;
+    11)  echo "Current cluster: $(kubectl config current-context)"
         kubectl version --short
         kubectl get "deployments,pods,services,ingress,secrets" --namespace=kube-system -o wide
         ;;
-    10)  kubectl get 'deployments,pods,services,ingress,secrets,persistentvolumeclaims,persistentvolumes,nodes' --namespace=fabricnlp -o wide
+    21)  kubectl get 'deployments,pods,services,ingress,secrets,persistentvolumeclaims,persistentvolumes,nodes' --namespace=fabricnlp -o wide
         ;;
-    12)  Write-Host "MySql root password: $(ReadSecretPassword mysqlrootpassword fabricnlp)"
+    23)  Write-Host "MySql root password: $(ReadSecretPassword mysqlrootpassword fabricnlp)"
             Write-Host "MySql NLP_APP_USER password: $(ReadSecretPassword mysqlpassword fabricnlp)"
             Write-Host "SendGrid SMTP Relay key: $(ReadSecretPassword smtprelaypassword fabricnlp)"
         ;;
-    13)  pods=$(kubectl get pods -n fabricnlp -o jsonpath='{.items[*].metadata.name}')
+    24)  pods=$(kubectl get pods -n fabricnlp -o jsonpath='{.items[*].metadata.name}')
         for pod in $pods
         do
                 Write-Output "=============== Pod: $pod ================="
+                kubectl describe pods $pod -n fabricnlp
                 kubectl logs --tail=20 $pod -n fabricnlp
         done
         ;;
     q) echo  "Sending SIGKILL signal" 
     ;;
-    *) echo "Signal number $1 is not processed"
+    *) echo "Menu item $1 is not known"
     ;;
     esac
 

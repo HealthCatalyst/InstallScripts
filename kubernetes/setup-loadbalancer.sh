a5119d61430c5cb3a4f7ce46be579b006d7ce726
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2018-03-20 20:01:49 -0700

    create self-signed certs in setup-loadbalancer
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- kubernetes/setup-loadbalancer.sh
+++ kubernetes/setup-loadbalancer.sh
@@ -9,7 +9,7 @@ GITHUB_URL="https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/mast
 source <(curl -sSL "$GITHUB_URL/kubernetes/common.sh?p=$RANDOM")
 # source ./kubernetes/common.sh
 
-version="2018.03.19.01"
+version="2018.03.20.01"
 
 echo "---- setup-loadbalancer.sh version $version ------"
 
@@ -29,7 +29,42 @@ customerid=$(ReadSecret "customerid")
 AskForSecretValue "dnshostname" "DNS name used to connect to the master VM "
 dnsrecordname=$(ReadSecret "dnshostname")
 
-yamlfile="kubernetes/loadbalancer/configmaps/config.yaml"
+sslsecret=$(kubectl get secret traefik-cert-ahmn -n kube-system --ignore-not-found=true)
+
+if [[ -z "$sslsecret" ]]; then
+
+        read -p "Location of SSL cert files (tls.crt and tls.key): (leave empty to use self-signed certificates) " certfolder < /dev/tty
+
+        if [[ -z "$certfolder" ]]; then
+                echo "Creating self-signed SSL certificate"
+                sudo yum -y install openssl
+                u="$(whoami)"
+                certfolder="/opt/healthcatalyst/certs"
+                echo "Creating folder: $certfolder and giving access to $u"
+                sudo mkdir -p "$certfolder"
+                sudo setfacl -m u:$u:rwx "$certfolder"
+                rm -rf "$certfolder/*"
+                cd "$certfolder"
+                # https://gist.github.com/fntlnz/cf14feb5a46b2eda428e000157447309
+                echo "Generating CA cert"
+                openssl genrsa -out rootCA.key 2048
+                openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 -subj /CN=HCKubernetes/O=HealthCatalyst/ -out rootCA.crt
+                echo "Generating certificate for $dnsrecordname"
+                openssl genrsa -out tls.key 2048
+                openssl req -new -key tls.key -subj /CN=$dnsrecordname/O=HealthCatalyst/ -out tls.csr
+                openssl x509 -req -in tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out tls.crt -days 3650 -sha256
+        fi
+
+        ls -al "$certfolder"
+
+        echo "Deleting any old TLS certs"
+        kubectl delete secret traefik-cert-ahmn -n kube-system --ignore-not-found=true
+
+        echo "Storing TLS certs as kubernetes secret"
+        kubectl create secret generic traefik-cert-ahmn -n kube-system --from-file="$certfolder/tls.crt" --from-file="$certfolder/tls.key"
+fi
+
+yamlfile="kubernetes/loadbalancer/configmaps/config.ssl.yaml"
 echo "Downloading $GITHUB_URL/$yamlfile"
 ReadYamlAndReplaceCustomer $GITHUB_URL "$yamlfile" $customerid \
         | kubectl apply -f -

cbc390b99b099207d7bb7c91a73bc87ba6972d34
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2018-03-27 19:51:33 -0700

    remove code to delete dashboard
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- kubernetes/dashboard/setup-kubdashboard.sh
+++ kubernetes/dashboard/setup-kubdashboard.sh
@@ -9,22 +9,23 @@ GITHUB_URL="https://raw.githubusercontent.com/HealthCatalyst/InstallScripts/mast
 source <(curl -sSL "$GITHUB_URL/kubernetes/common.sh?p=$RANDOM")
 # source ./kubernetes/common.sh
 
-version="2018.03.27.02"
+version="2018.03.27.03"
 
 echo "---- setup-kubdashboard.sh version $version ------"
 
 # enable running pods on master
 # kubectl taint node mymasternode node-role.kubernetes.io/master:NoSchedule
 
-echo "checking if there is an existing dashboard"
-dashboards="$(kubectl -n kube-system get pod -o name --ignore-not-found=true | grep dashboard)"
+# echo "checking if there is an existing dashboard"
+# dashboards="$(kubectl -n kube-system get pod -o name --ignore-not-found=true | grep dashboard)"
 
-if [[ ! -z "$dashboards" ]]; then
-    echo "Deleting existing dashboard"
-    kubectl -n kube-system delete $dashboards 
-else
-    echo "No existing dashboard found"
-fi
+# if [[ ! -z "$dashboards" ]]; then
+#     echo "Deleting existing dashboard"
+#     kubectl -n kube-system delete $dashboards 
+# else
+#     echo "No existing dashboard found"
+# fi
+
 # kubectl delete 'pods,services,configMaps,deployments,ingress' -l k8s-traefik=traefik -n kube-system --ignore-not-found=true
 
 # kubectl delete ServiceAccount traefik-ingress-controller-serviceaccount -n kube-system --ignore-not-found=true

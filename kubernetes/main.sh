57b8a1aff01646049843a339588a2e29c5bcf370
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-04-12 14:33:07 -0700

    fix url redirection
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- kubernetes/main.sh
+++ kubernetes/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "This has moved to curl -sSL https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/kubernetes/main.sh | bash"
+echo "This has moved to curl -sSL https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/onprem/main.sh | bash"
